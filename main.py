15a2b97ae0accb7a3d06f27abb68293e0c330ae5
jonnyACCI <jshan9078@gmail.com>
2024-01-27 18:10:40 -0500

    added leaderboard page

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from taipy.gui import Gui
 from pages.landing import landing_page
 from pages.pacmanpage import pacman_page
 from pages.flappybirdpage import flappybird_page
+from pages.leaderboardpage import leaderboard_page
 
 pages = {
  "/": "<center><|navbar|></center>",
 "landing": landing_page,
 "pacman": pacman_page,
-"flappybird": flappybird_page
+"flappybird": flappybird_page,
+"leaderboards": leaderboard_page
 }
 
 
---
+++ pages/leaderboardpage.py
@@ -0,0 +1,13 @@
+from taipy.gui import Gui, Markdown
+
+
+title="Global Leaderboards"
+
+
+# Definition of the page
+leaderboard_page = Markdown("""
+<|{title}|id=title|>
+
+""")
+
+
